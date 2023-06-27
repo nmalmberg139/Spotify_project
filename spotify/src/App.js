94ebcdbbf4aaac9d794d85218c050a2964194b5d
nmalmberg139 <nmalmberg01@aurora.edu>
2023-06-27 16:37:51 -0500

    Create and enhance DataLayer structure to token

--- spotify/src/App.js
+++ spotify/src/App.js
@@ -1,11 +1,19 @@
-import React, { useEffect, useState  } from 'react';
+import React, { useEffect,} from 'react';
 import './App.css';
 import Login from './Login';
+import Player from './Player';
 import { getTokenFromUrl } from './Spotify';
+import SpotifyWebApi from "spotify-web-api-js";
+import { useDataLayerValue } from './DataLayer';
 
-// 1:17:17
+// 2:16:06
+
+// communicating with Spotify API
+const spotify = new SpotifyWebApi();
+
 function App() {
-  const [token, setToken] = useState(null);
+  // const [token, setToken] = useState(null);
+  const [{ user, token },  dispatch] = useDataLayerValue();
 
   // run code based on a given condition
   useEffect( () =>{
@@ -14,13 +22,29 @@ function App() {
      const _token = hash.access_token
 
      if (_token){
-       setToken(_token)
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      })
+      //  setToken(_token)
+
+       // getting the token to the spotify api from react
+       spotify.setAccessToken(_token);
+
+
+       spotify.getMe().then(user =>{
+        // Pop in a user from the dataLayer component and reading the data
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        })
+       })
      }
-     console.log('I have a token', token);
   }, []);
+
   return (
     <div className="app">
-      { token ?  <h1>I am logged in</h1> : <Login /> }
+      { token ?  <Player spotify={spotify} /> : <Login /> }
 
 
     </div>
